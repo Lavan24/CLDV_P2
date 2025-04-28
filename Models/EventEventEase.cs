335124f1daddcb5d319da10988b0afff327dbf1b
Lavan24 <ST10438009@vcconnect.edu.za>
2025-04-28 14:31:28 +0200

    Layouts and Requirements.

--- Models/EventEventEase.cs
+++ Models/EventEventEase.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventEaseApplication.Models;
 
 public partial class EventEventEase
 {
+    [Required]
     public int EventId { get; set; }
 
+    [Required]
     public string? EventName { get; set; }
 
+    [Required]
     public DateOnly? EventDate { get; set; }
 
+    [Required]
     public TimeOnly? Event_Time { get; set; }
 
+    [Required]
     public string? EventDescription { get; set; }
 
     public virtual ICollection<BookingEventEase> BookingEventEases { get; set; } = new List<BookingEventEase>();
--- Models/VenueEventEase.cs
+++ Models/VenueEventEase.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventEaseApplication.Models;
 
 public partial class VenueEventEase
 {
+    [Required]
     public int VenueId { get; set; }
 
+    [Required]
     public string? VenueName { get; set; }
 
+    [Required]
     public string? VenueLocation { get; set; }
 
+    [Required]
     public int? VenueCapacity { get; set; }
 
+    [Required]
     public string? ImageUrl { get; set; }
 
     public virtual ICollection<BookingEventEase> BookingEventEases { get; set; } = new List<BookingEventEase>();
